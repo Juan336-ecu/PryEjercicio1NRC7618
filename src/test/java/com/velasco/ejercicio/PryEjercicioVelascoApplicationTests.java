cf9e57d39c4df0ac78f9e1613332ce78cca88d94
Juan336-ecu <jcvelasco3@espe.edu.ec>
2020-06-02 23:01:39 -0500

    Codigo Inicial

---
+++ src/test/java/com/velasco/ejercicio/PryEjercicioVelascoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velasco.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicioVelascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
